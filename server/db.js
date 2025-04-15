2c2fe0ca82d46ad2a27cdd10227f0655f055bd8a
leecan95 <leecankun@gmail.com>
2025-04-15 16:52:16 +0700

    edit server

--- server/db.js
+++ server/db.js
@@ -2,8 +2,8 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  host: process.env.DB_HOST || 'localhost',
+  password: process.env.DB_PASSWORD || 'cancaucacan',
+  host: process.env.DB_HOST || 'postgre-db.craw4ikasnx6.ap-southeast-2.rds.amazonaws.com',
   port: process.env.DB_PORT || 5432,
   database: process.env.DB_NAME || 'auth_db'
 });
